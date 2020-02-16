fe793c34c47c6c766391df78e115f1c28ac9ade4
Josh Jacobsen <joshj@entouch.net>
2020-02-15 20:15:52 -0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+#Create a class to store all the game settings
+class Settings:
+    #initialize game settings
+    def __init__(self):
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color (230,230,230)
