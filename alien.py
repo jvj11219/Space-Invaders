7104bb66afe872156ca97b2afdc9723fb065f1b1
Josh Jacobsen <joshj@entouch.net>
2020-03-01 21:46:06 -0600

    Add Additional Alien Movement

--- alien.py
+++ alien.py
@@ -16,3 +16,14 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
         #Store the alien's horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        # Move the alien to the right by alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        # Return True if alien is at edge of screen
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #Create a class to store all the game settings
 class Settings:
-    #initialize game settings
+    #Initialize game settings
     def __init__(self):
         #Screen settings
         self.screen_width = 1200
@@ -14,3 +14,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60) #Dark gray
         self.bullets_allowed = 3
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #Fleet_direction of 1 represents right and -1 represents left
+        self.fleet_direction = 1
