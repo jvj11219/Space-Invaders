75dbd59fabb99a3a50f5042fd6c511284decdf8c
Josh Jacobsen <joshj@entouch.net>
2020-03-01 23:06:48 -0600

    Reset Game on Button Click

--- settings.py
+++ settings.py
@@ -7,10 +7,10 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230,230,230)
         #Ship settings
-        self.ship_speed = 20
+        self.ship_speed = 1.5
         self.ship_limit = 3
         #Bullet settings
-        self.bullet_speed = 30.0
+        self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60) #Dark gray
@@ -20,3 +20,20 @@ class Settings:
         self.fleet_drop_speed = 10
         #Fleet_direction of 1 represents right and -1 represents left
         self.fleet_direction = 1
+        #How quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        #Initialize settings that change throughout the game
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+        #Fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        #Increase speed settings
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
